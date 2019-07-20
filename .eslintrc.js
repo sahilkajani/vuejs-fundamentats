11a178e12d7680829eac6b0ef00caaf67ed25f70
Sahil <sahilkajani30@gmail.com>
2019-07-20 21:05:36 +0100

    Initial Commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,4 +14,7 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
+  rules : {
+    "linebreak-style": 0
+  }
 };
---
+++ src/home/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home{
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+</style>
